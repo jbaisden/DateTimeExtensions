a317568c4df2d1484c5e5269a50974a22d9d6f27
Jason Baisden <jasonbaisden@gmail.com>
2015-03-01 20:57:16 -0600

    IsBefore, IsAfter, IsBetween DateTime Extensions

---
+++ ExtensionsPlayground/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsPlayground.Extensions
+{
+
+    public interface DateCalcs
+    {
+        DateTime min { get; set; }
+        DateTime max { get; set; }
+
+        DateTime GetMinDate();
+        DateTime GetMaxDate();
+    }
+
+    //public abstract class AbDateCalc : DateCalcs
+    //{
+
+    //}
+
+
+
+    public static class DateTimeExtensions
+    {
+
+        public enum DateComparisonResult
+        {
+            IsPast = -1,
+            IsFuture = 1,
+            TheSame = 0
+        }
+
+        public static bool IsBefore(this DateTime date, DateTime comparisonDate)
+        {
+            if (date.CompareTo(comparisonDate) == (int)DateComparisonResult.IsPast) return true;
+
+            return false;
+        }
+
+        public static bool IsAfter(this DateTime date, DateTime comparisonDate)
+        {
+            if (date.CompareTo(comparisonDate) == (int)DateComparisonResult.IsFuture) return true;
+
+            return false;
+        }
+
+        public static bool IsBetween(this DateTime date, DateTime oldestDate, DateTime nextOldest)
+        {
+
+            if (date.IsAfter(oldestDate) && date.IsBefore(nextOldest)) return true;
+
+            return false;
+        }
+
+        public static bool IsDateWithinXRangeOfAnotherDate(this DateTime date, int interval, DateInterval dateInterval, DateTime comparisonDate)
+        {
+            DateTime _min = comparisonDate;
+            DateTime _max = comparisonDate;
+
+            switch(dateInterval)
+            {
+                case DateInterval.Days:
+                    _min = _min.AddDays(-interval);
+                    _max = _max.AddDays(interval); 
+                    Console.WriteLine(
+                        string.Format("Min Date is {0} Max Date is {1}",
+                            _min.ToShortDateString(),
+                            _max.ToShortDateString()));
+                    break;
+                case DateInterval.Months:
+                    _min = _min.AddMonths(-interval);
+                    _max = _max.AddMonths(interval);
+                    Console.WriteLine(
+                        string.Format("Min Date is {0} Max Date is {1}",
+                            _min.ToShortDateString(),
+                            _max.ToShortDateString()));
+                    break;
+                case DateInterval.Years:
+                    _min = _min.AddYears(-interval);
+                    _max = _max.AddYears(interval);
+                    Console.WriteLine(
+                        string.Format("Min Date is {0} Max Date is {1}",
+                            _min.ToShortDateString(),
+                            _max.ToShortDateString()));
+                    break;
+            }
+
+            return _min <= date && date <= _max;
+        }        
+    }
+
+    public enum DateInterval
+    {
+        Days,
+        Months,
+        Years
+    }
+
+}
---
+++ ExtensionsPlayground/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionsPlayground.Extensions;
+
+namespace ExtensionsPlayground
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            DateTime past = now.AddMonths(-1);
+            DateTime future = now.AddMonths(1);
+            
+            Console.WriteLine(
+                string.Format("{0} is before {1} is {2} (should be false)",
+                    now.ToShortDateString(), past.ToShortDateString(), now.IsBefore(past)
+                ));
+
+            Console.WriteLine(
+                string.Format("{0} is before {1} is {2} (should be true)",
+                    now.ToShortDateString(), past.ToShortDateString(), now.IsBefore(future)
+                ));
+
+            Console.WriteLine(
+                string.Format("{0} is after {1} is {2} (should be false)",
+                    past.ToShortDateString(), now.ToShortDateString(), past.IsAfter(now)
+                ));
+
+            Console.WriteLine(
+                string.Format("{0} is after {1} is {2} (should be true)",
+                    now.ToShortDateString(), past.ToShortDateString(), now.IsAfter(past)
+                ));
+
+            Console.WriteLine(
+                string.Format("{0} is between {1} and {2} is {3} (should be false)",
+                    past.ToShortDateString(), now.ToShortDateString(), future.ToShortDateString(), past.IsBetween(now, future)
+                ));
+
+            Console.WriteLine(
+                string.Format("{0} is between {1} and {2} is {3} (should be true)",
+                    now.ToShortDateString(), past.ToShortDateString(), future.ToShortDateString(), now.IsBetween(past,future))
+                );
+
+            Console.ReadLine();
+        }
+
+
+        static void Main2(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            DateTime past = new DateTime(2015, 1, 15);
+            DateTime future = new DateTime(2015, 3, 15);
+            DateTime comparison = now.AddDays(-2);
+            int interval = 1;
+            DateInterval di = DateInterval.Days;
+
+            Console.WriteLine(
+                string.Format("Now, {0}, is with in {1} {2} of {3} is {4}",
+                    now.ToShortDateString(),
+                    interval.ToString(),
+                    di.ToString(),
+                    comparison.ToShortDateString(),
+                    now.IsDateWithinXRangeOfAnotherDate(interval, di, comparison).ToString())
+            );
+
+            Console.ReadLine();
+        }
+
+    }
+
+
+
+   
+
+}
